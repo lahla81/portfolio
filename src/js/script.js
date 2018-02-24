ad77eed3d867cfad8aa950fadfc0ad60b2e6b4f1
lahla81 <lahla81@yahoo.com>
2018-02-24 22:09:28 +0200

    finishing of page

--- src/js/script.js
+++ src/js/script.js
@@ -13,41 +13,26 @@ var wow = new WOW({
 });
 wow.init();
 
-// لإخفاء القائمة عند النقر علي أي رابط
-$(".navbar-nav li a").click(function(event) {
-    $(".navbar-collapse").collapse('hide');
-  });
-
-//   لعمل حركة عند الإنتقال عبر الصفحة بإستخدام الروابط
-$("a.scroll").on('click', function(event){
-    var hash = this.hash;
-    $('html, body').animate({scrollTop: $(hash).offset().top},800,function(){});
-});
-
-    // E-mail validation 
-    $(".custom-form-control").blur(function(){
-        var x = document.forms["myForm"]["email"].value;
-        var atpos = x.indexOf("@");
-        var dotpos = x.lastIndexOf(".");
-        if(atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length){
-            $(this).parent().find(".alert-danger").fadeIn(200);
-        }
-        else{
-            $(this).parent().find(".alert-success").fadeIn(200);
-        }
+    // لإخفاء القائمة عند النقر علي أي رابط
+    $(".navbar-nav li a").click(function(event) {
+        $(".navbar-collapse").collapse('hide');
     });
 
-    // E-mail validation1 
-    $(".custom-form-control").blur(function(){
-        var x = document.forms["myForm1"]["email1"].value;
-        var atpos = x.indexOf("@");
-        var dotpos = x.lastIndexOf(".");
-        if(atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length){
-            $(this).parent().find(".alert-danger").fadeIn(200);
-        }
-        else{
-            $(this).parent().find(".alert-success").fadeIn(200);
-        }
+    //   لعمل حركة عند الإنتقال عبر الصفحة بإستخدام الروابط
+    $("a.scroll").on('click', function(event){
+        var hash = this.hash;
+        $('html, body').animate({scrollTop: $(hash).offset().top},800,function(){});
+    });
+
+    $('.circle').circleProgress({
+        startAngle: -Math.PI / 2,
+        fill: {color: '#0575e6'}
+        }).on('circle-animation-progress', function(event, progress, stepValue) {
+            $(this).find('span').html(Math.round(stepValue * 100) + '%');
+    });
+
+    $(function(){
+        $("#contact-form").validate();
     });
 });
 
